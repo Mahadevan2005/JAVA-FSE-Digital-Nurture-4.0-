464a6d231c4da4f158e8cf6208678d7e15c6c676
M.Mahadevan <mahadevan122005@gmail.com>
2025-05-25 10:12:43 +0530

    Latest changes with Java project

---
+++ src/Module-3/Core - Java/Tasks/ArrayListExample.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        ArrayList<String> al = new ArrayList<>();
+        String c = "Y";
+        while(c != "N")
+        {
+            System.out.print("Type 'Y' to start entering the name: ");
+            c = s.nextLine();
+            if(c.equals("N"))
+                break;
+            System.out.print("Enter the student name: ");
+            String temp = s.nextLine();
+            al.add(temp);
+        }
+        if(al.size() > 0)
+        {
+            System.out.println("---Entered names are---");
+            for(int i=0;i<al.size();i++)
+            {
+                System.out.println(al.get(i));
+            }
+        }
+    }
+}
